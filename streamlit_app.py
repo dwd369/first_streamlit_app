75341e69ad37971f8ba37c5ce9fbf1498c3ac1bc
David Deng <dwd7290@gmail.com>
2024-01-16 12:51:59 -0800

    get json file from API request body

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,8 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # get fruit list
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.text(fruityvice_response)
+
+streamlit.text(fruityvice_response.json())
 
 # let's pu a pick list here so they can pick the fruits they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
