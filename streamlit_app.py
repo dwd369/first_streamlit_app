bd776e1417900ceb4e92b253d35095935c8c08a8
David Deng <dwd7290@gmail.com>
2024-01-16 12:50:15 -0800

    change request location

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,11 @@ streamlit.text('🥗 Kale, Spinach * Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.text(fruityvice_response)
 
 # get fruit list
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.text(fruityvice_response)
 
 # let's pu a pick list here so they can pick the fruits they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
